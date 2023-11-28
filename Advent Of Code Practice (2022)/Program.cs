4b8d22e3f29fe62a74e956afbfd993d087f8ea89
23JA1168 <23JA1168@L2768>
2023-11-28 11:32:28 +0000

    Add project files.

---
+++ Advent Of Code Practice (2022)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_Practice__2022_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
