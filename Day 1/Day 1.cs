92b66c6492783e8629adc78c3f9324ca797241fe
23JA1168 <23JA1168@L2768>
2023-11-28 11:57:32 +0000

    tast

---
+++ Day 1/Day 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1
+{
+    internal class Program
+    {
+        static int PerElfCals(char[] cals)
+        {
+            for (int i = 0; i < ; i++) { }
+        }
+
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                }
+            }
+
+        }
+    }
+}
