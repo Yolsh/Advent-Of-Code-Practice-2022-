028fc684beac709c3c89b80315f968cdcbe9f3fd
23JA1168 <23JA1168@L3104>
2023-11-30 12:36:27 +0000

    test

--- Day 1/Day 1.cs
+++ Day 1/Day 1.cs
@@ -4,24 +4,52 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Day_1
 {
     internal class Program
     {
-        static int PerElfCals(char[] cals)
+        static int PerElfCals(string[] cals)
         {
-            for (int i = 0; i < ; i++) { }
+            int total = 0;
+            for (int i = 0; i < cals.Length; i++)
+            {
+                total += int.Parse(cals[i]);
+            }
+            return total;
+        }
+
+        static void find(List<int> list)
+        {
+            int largest = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > largest)
+                {
+                    largest = list[i];
+                }
+            }
+            Console.WriteLine(largest);
+            Console.ReadKey();
         }
 
         static void Main(string[] args)
         {
+            List<int> totalCals = new List<int>();
             using (StreamReader sr = new StreamReader("input.txt"))
             {
                 while (!sr.EndOfStream)
                 {
-
+                    List<string> elf = new List<string>();
+                    string line = sr.ReadLine();
+                    while (line != "")
+                    {
+                        elf.Add(line);
+                    }
+                    totalCals.Add(PerElfCals(elf.ToArray()));
                 }
+                find(totalCals);
             }
 
         }
